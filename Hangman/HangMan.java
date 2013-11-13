31ffb30d558794008a530321236c339c53fb509a
Zoryn76 <firedragonid4@yahoo.com>
2013-11-12 17:48:39 -0700

    update code to fix an issue
    removed duplicate method, updated code that verified if the word entered
    was correct. Code was still returning a false win if the first letter
    matched even if the rest of the word was wrong.

--- Hangman/HangMan.java
+++ Hangman/HangMan.java
@@ -25,6 +25,7 @@ public class HangMan
          char gword[] = new char[word.length];
          intializeGWord(gword);//sets gword to underscores
          char letter = ' ';
+         boolean win = false;
          Scanner input = new Scanner(System.in);
          System.out.println("Welcome to hangman.");
       
@@ -34,40 +35,32 @@ public class HangMan
             printHangman(incorrect);
             printGuessWord(gword);
             System.out.println();
-            System.out.print("Please input a letter: ");
-            String myWord = input.next();//searches word for the letter guessed
-            letter = myWord.charAt(0);
-            
-            if (myWord.length() == word.length)
+            System.out.print("Please input a letter or word: ");
+            char myWord[] = input.next().toCharArray();//allows letter or word to be entered
+            letter = myWord[0];//gets first letter entered from myWord
+           
+            if (myWord.length == word.length)//checks if word entered matches
             {
-                char c;
-                int count;
-                char arr[] = new char [word.length];
-                c = letter;
-                count = 0;
-                while (count < word.length)
-                {
-                arr[count] = c;
-                ++count;
-                }
-                char win1 = wholeWord(word, arr);
-                if (win1 == 'y')
-                   break;
-                gword = word;
-                break;
+               win = checkWin(word, myWord);
+               if (win == false)
+               incorrect ++;
             }
-            
-            //false if letter not in word, else adds the letter to gword
-            if (cLetter == false)
-            {
-               System.out.println();
-               System.out.printf("Sorry, %c is not in the word\n", letter);
-               incorrect += 1;
-            }
-               else addLetter(word, gword, letter);
-      
-            //checks for win and breaks out of loop if player won   
-            boolean win = checkWin(word, gword);
+               else//if only letter entered checks if the letter is in the word
+               {
+                  //false if letter not in word, else adds the letter to gword
+                  boolean cLetter = linearSearch(word, letter);
+                  if (cLetter == false)
+                  {
+                     System.out.println();
+                     System.out.printf("Sorry, %c is not in the word\n", letter);
+                     incorrect ++;
+                  }
+                     else 
+                     {
+                        addLetter(word, gword, letter);
+                        win = checkWin(word, gword);
+                     }                        
+               }      
             if (win == true)
                break;         
          }while (incorrect < 6);//loops ends when player guesses incorrectly 6 times 
@@ -85,7 +78,7 @@ public class HangMan
             else
             {
                printHangman(incorrect);
-               printGuessWord(gword);
+               printGuessWord(word);
                System.out.println();
                System.out.println("Congratulations you WIN!!!!!!");
             }//end  else
@@ -168,12 +161,4 @@ public class HangMan
          list[i] = '_';
    }//end intializeGWord
     
-    //Hi Seth started this method to allow users to guess the whole word.
-    public static char wholeWord(char[] list, char wlist[])
-   {
-       for (int i = 0; i < list.length; i++)
-         if (list != wlist)
-             return 'n';
-        return 'y';
-   }//end WholeWord
 }//end HangMan
